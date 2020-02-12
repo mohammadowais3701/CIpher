370a3a9076815acaee5af5d5bb3effe61d995ada
mohammadowais3701 <mohammadowais3701@gmail.com>
2020-02-12 11:23:56 +0500

    Cipher Implementation on windows form

---
+++ plain_textTo_ciphertext/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace plain_textTo_ciphertext
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.plaintxtbox = new System.Windows.Forms.TextBox();
+            this.ciphrtxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.keytxt = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plain";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(67, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cipher";
+            // 
+            // plaintxtbox
+            // 
+            this.plaintxtbox.BackColor = System.Drawing.Color.Yellow;
+            this.plaintxtbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plaintxtbox.Location = new System.Drawing.Point(167, 57);
+            this.plaintxtbox.Name = "plaintxtbox";
+            this.plaintxtbox.Size = new System.Drawing.Size(185, 29);
+            this.plaintxtbox.TabIndex = 2;
+            // 
+            // ciphrtxt
+            // 
+            this.ciphrtxt.BackColor = System.Drawing.Color.Yellow;
+            this.ciphrtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ciphrtxt.Location = new System.Drawing.Point(167, 108);
+            this.ciphrtxt.Name = "ciphrtxt";
+            this.ciphrtxt.Size = new System.Drawing.Size(185, 29);
+            this.ciphrtxt.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(146, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "CONVERT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(67, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Key";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Yellow;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(67, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Algorithm";
+            // 
+            // keytxt
+            // 
+            this.keytxt.BackColor = System.Drawing.Color.Yellow;
+            this.keytxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keytxt.Location = new System.Drawing.Point(167, 158);
+            this.keytxt.Name = "keytxt";
+            this.keytxt.Size = new System.Drawing.Size(185, 29);
+            this.keytxt.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Yellow;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Shift",
+            "Vigenere",
+            "PlayFair"});
+            this.comboBox1.Location = new System.Drawing.Point(167, 201);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(185, 32);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::plain_textTo_ciphertext.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(617, 297);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.keytxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ciphrtxt);
+            this.Controls.Add(this.plaintxtbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox plaintxtbox;
+        private System.Windows.Forms.TextBox ciphrtxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox keytxt;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ plain_textTo_ciphertext/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace plain_textTo_ciphertext
+{
+    public partial class Form1 : Form
+    {
+        List<Char> alphabet = new List<char>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Char c1 = 'A';
+            int n,k=32,y;
+            for (int i = 0; i <=25; i++)
+            {
+                alphabet.Add(c1);
+                c1++;
+            }
+            if (comboBox1.Text == "Shift")
+            { 
+                if (plaintxtbox.Text != "")
+                {
+                    try
+                    {
+                        string str = (keytxt.Text).ToUpper();
+                        n = alphabet.IndexOf(str[0]);
+                  
+                        
+                        ciphrtxt.Text = "";
+                        String txt = plaintxtbox.Text.ToUpper();
+                        for (int i = 0; i < (plaintxtbox.Text).Length; i++)
+                        {
+                            
+                          
+                            if (Convert.ToInt16(txt[i]) != 32)
+                            {
+                                y = alphabet.IndexOf(txt[i]);
+                                k = (y + n) % 26;
+                                ciphrtxt.Text += alphabet[k];
+                            }
+                            else
+                            {
+                                ciphrtxt.Text += " ";
+                            }
+                            //  if (!((k >= 65 && k <= 90) || (k >= 97 && k <= 122)))
+                          
+
+                          
+                        }
+                       
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Something went wrong");
+                    }
+                }
+                else if (ciphrtxt.Text != "")
+                {
+                    try
+                    {
+                        string str = (keytxt.Text).ToUpper();
+                         n = alphabet.IndexOf((str[0]));
+                       
+                        plaintxtbox.Text = "";
+                        String txt = ciphrtxt.Text.ToUpper();
+                        for (int i = 0; i < (ciphrtxt.Text).Length; i++)
+                        {
+                            if (Convert.ToInt16(txt[i]) != 32)
+                            {
+                                y = alphabet.IndexOf(txt[i]);
+                                k = (y - n);
+                                if (k < 0)
+                                    k = (26 + k) % 26;
+                                //   if (!((k >= 65 && k <= 90) || (k >= 97 && k <= 122)))
+                                // k = k + 25;
+                                plaintxtbox.Text += alphabet[k];
+                            }
+                            else
+                            {
+                                plaintxtbox.Text += " ";
+                            }
+                        }
+                       
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Something went wrong");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No Text is available");
+
+                }
+        }
+            else if (comboBox1.Text == "Vigenere")
+            {
+                int keyindex;
+                if (plaintxtbox.Text != "")
+                {
+                    try
+                    {
+                        string str = (keytxt.Text).ToUpper();
+                        
+                  
+
+                        ciphrtxt.Text = "";
+                        String txt = plaintxtbox.Text.ToUpper();
+                        for (int i = 0; i < (plaintxtbox.Text).Length; i++)
+                        {
+                            if (Convert.ToInt16(txt[i]) != 32)
+                            {
+                                keyindex = i % (str.Length);
+                                n = alphabet.IndexOf(str[keyindex]);
+                                y = alphabet.IndexOf(txt[i]);
+                                k = (y + n) % 26;
+                                ciphrtxt.Text += alphabet[k];
+                                //  if (!((k >= 65 && k <= 90) || (k >= 97 && k <= 122)))
+                            }
+                            else
+                            {
+                                ciphrtxt.Text += " ";
+                            }
+                            
+                        }
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Something went wrong");
+                    }
+                }
+                else if (ciphrtxt.Text != "")
+                {
+                    try
+                    {
+                        string str = (keytxt.Text).ToUpper();
+                       
+
+                        plaintxtbox.Text = "";
+                        String txt = ciphrtxt.Text.ToUpper();
+                        for (int i = 0; i < (ciphrtxt.Text).Length; i++)
+                        {
+                            if (Convert.ToInt16(txt[i]) != 32)
+                            {
+                                keyindex = i % (str.Length);
+                                n = alphabet.IndexOf(str[keyindex]);
+                                y = alphabet.IndexOf(txt[i]);
+                                k = (y - n);
+                                if (k < 0)
+                                    k = (26 + k) % 26;
+
+                                plaintxtbox.Text += alphabet[k];
+                            }
+                            {
+                                plaintxtbox.Text += " ";
+                            }
+
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Something went wrong");
+                    }
+                }
+
+
+            }
+            else if (comboBox1.Text == "PlayFair")
+            {
+
+
+                int keyindex,len,l=-1;
+                if (plaintxtbox.Text != "")
+                {
+                    try
+                    {
+                        string str = (keytxt.Text).ToUpper();
+                     
+                        //   char[] playfair = new char[25];
+                        List<char> playfair = new List<char>();
+                        char ch = 'A';
+                        len = str.Length;
+
+                        char[,] playfairs = new char[5, 5];
+                        for(int i = 0; i < len; i++)
+                        {
+                            if (Convert.ToInt16(str[i]) != 32 && str[i]!='J')
+                            {
+                                if (!playfair.Contains(str[i]))
+                                {
+                                    playfair.Add(str[i]);
+                                    l++;
+                                }
+                            }
+                        }
+                   
+                        for(int j = 0; j < 26; j++)
+                        {
+                           
+                            if (!playfair.Contains(ch) && ch!='J')
+                            {
+                                playfair.Add(ch);
+                            }
+                          
+                            ch++;
+                        }
+                        /* for(int i = 0; i < playfair.Count; i++)
+                         {
+                             MessageBox.Show(Convert.ToString(playfair[i]));
+                         }*/
+                        int h = 0,w1=0,x1=0,w2=0,x2=0,c=0;
+                
+                        for(int i = 0; i < 5; i++)
+                        {
+                            for(int j = 0; j < 5; j++)
+                            {
+                                playfairs[i, j] = playfair[h];
+                              
+                                h++;
+                            }
+                        }
+                        
+                        ciphrtxt.Text = "";
+                        string pt = plaintxtbox.Text.Replace(" ", "");
+                        pt=pt.Replace("J", "K");
+                        for ( int i = 0; i < pt.Length; i++)
+                        {
+                            if(!((pt[i]>='A' && pt[i]<='Z')|| (pt[i]>='a' && pt[i] <= 'z')))
+                            {
+                                pt = pt.Remove(i);
+                               
+                            }
+                        }
+                        if (pt.Length % 2 != 0)
+                        {
+                            pt += 'X';
+                        }
+                        
+                        for (int index = 0; index < pt.Length; index ++)
+                        {
+                            for (int i = 0; i < 5; i++)
+                            {
+                                for (int j = 0; j < 5; j++)
+                                {
+                                    if (pt[index] == playfairs[i, j])
+                                    {
+                                        if (c == 0)
+                                        {
+                                            w1 = i;
+                                            x1 = j;
+                                        }
+                                        else if (c == 1)
+                                        {
+                                            w2 = i;
+                                            x2 = j;
+                                        }
+                                        c++;
+                                    }
+
+                                }
+                            }
+                            if (c == 2)
+                            {
+                                c = 0;
+                                if (w1 == w2)
+                                {
+                                    x2++;
+                                    x1++;
+                                    ciphrtxt.Text += playfairs[w1, (x1 % 5)];
+                                          ciphrtxt.Text += playfairs[w1, (x2 % 5)];
+                                }
+                                else if (x1 == x2)
+                                {
+                                    w1++;
+                                    w2++;
+                                    ciphrtxt.Text += playfairs[(w1 % 5), x1];
+                                     ciphrtxt.Text += playfairs[(w2%5), x1];
+
+                                }
+                                else
+                                {
+                                    ciphrtxt.Text += playfairs[w1, x2];
+                                    ciphrtxt.Text += playfairs[w2,x1];
+                                }
+                            }
+                            
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Something went wrong");
+                    }
+                }
+
+
+            }
+            else if(comboBox1.Text=="") { MessageBox.Show("Select Algorithm "); }
+            }
+        }
+    
+}
